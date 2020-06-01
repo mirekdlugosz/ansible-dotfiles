cef038ef559cda83b31c8c66a232c63ea03b5fd1
Mirek Długosz <mirek@mirekdlugosz.com>
2020-06-01 10:57:42 +0200

    try to bring back some sanity to shell init files

--- roles/shell/files/shell/00_read_profile.bash
+++
@@ -1,9 +0,0 @@
-# force reading ~/.profile and ~/.bash_profile, in case they changed
-# since last login
-
-for i in "$HOME"/.profile "$HOME"/.bash_profile; do
-    if [ -r "$i" ]; then
-        . "$i"
-    fi
-done
-unset i
