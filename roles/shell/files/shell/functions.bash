cc1137e3aad71443bf99e5d378923ca19869b60d
Mirek Długosz <mirek@mirekdlugosz.com>
2021-07-30 11:50:25 +0200

    Add helper to set up pyenv environment for current shell
    I don't use pyenv nearly enough to pay performance price every time I
    open new shell. Usually I need pyenv only to build specific python
    version, which I then use to create venv which will be auto-activated by
    direnv.

--- roles/shell/files/shell/functions.bash
+++ roles/shell/files/shell/functions.bash
@@ -8,3 +8,11 @@ kopiuj() {
 	xclip -selection clipboard "$1"
 }
 
+pyenv_setup() {
+    export PYENV_ROOT="$HOME/.pyenv"
+    if [ ! -d "$PYENV_ROOT" ]; then
+        git clone https://github.com/pyenv/pyenv.git "$PYENV_ROOT"
+    fi
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init - bash)"
+}
--- roles/shell/files/shell/pyenv.bash
+++
@@ -1,3 +0,0 @@
-if echo "$PATH" |grep -q "/.pyenv/" && which pyenv >/dev/null 2>&1; then
-    eval "$(pyenv init - bash --no-rehash)"
-fi
