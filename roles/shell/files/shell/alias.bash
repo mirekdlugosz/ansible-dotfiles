b13ad6514b1bf65832cd63569d95b3418d7c5076
Mirek Długosz <mirek@mirekdlugosz.com>
2022-02-03 00:28:32 +0100

    Make it easier to start tmux

--- roles/shell/files/shell/alias.bash
+++ roles/shell/files/shell/alias.bash
@@ -4,4 +4,5 @@ alias mplayer=mpv
 #alias restart_plasma='kbuildsycoca4 ; sleep 0.5 ; kquitapp plasma-desktop ; sleep 0.5 ; kstart plasma-desktop'
 alias ls='ls --color=auto --hide="__pycache__" --hide="*.pyc"'
 alias grep='grep --colour=auto -I'
+alias ll='tmux'
 [ "$TERM" = 'rxvt-256color' ] && alias vim='TERM=xterm vim'
