8b5acab9f119ff9be8fdd47258bbaca747d943f3
Mirek Długosz <mirek@mirekdlugosz.com>
2023-01-05 11:04:17 +0100

    cmp-nvim-lsp deprecated update_capabilities

--- roles/nvim/templates/lua/30_plugin_opts.lua
+++ roles/nvim/templates/lua/30_plugin_opts.lua
@@ -315,7 +315,7 @@ if status_ok then
         'confirm_done',
         cmp_autopairs.on_confirm_done()
     )
-    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
 end
 
 -- >>>
