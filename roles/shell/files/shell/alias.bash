4b59b6dea835c217c4af0554d3129972ba644835
Mirek Długosz <mirek@mirekdlugosz.com>
2024-10-26 00:00:30 +0200

    Remove aliases that became outdated decades ago

--- roles/shell/files/shell/alias.bash
+++ roles/shell/files/shell/alias.bash
@@ -1,8 +1,4 @@
 # aliasy 
 alias nc=ncmpcpp
-alias mplayer=mpv
-#alias restart_plasma='kbuildsycoca4 ; sleep 0.5 ; kquitapp plasma-desktop ; sleep 0.5 ; kstart plasma-desktop'
 alias ls='ls --color=auto --hide="__pycache__" --hide="*.pyc"'
 alias grep='grep --colour=auto -I'
-alias ll='tmux'
-[ "$TERM" = 'rxvt-256color' ] && alias vim='TERM=xterm vim'
