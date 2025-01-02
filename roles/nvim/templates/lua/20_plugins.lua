2c0a25141a85d8aff23a4e509fe7b89eadfce9f7
Mirek Długosz <mirek@mirekdlugosz.com>
2025-01-02 16:09:21 +0100

    nvim: change plugin manager to paq
    packer.nvim stopped being maintained in mid-2023. It still works, but
    obviously it's better to use maintained software than unmaintained
    software.
    
    I tried moving to lazy.nvim the other day, but it was a disaster,
    requiring way too much work.
    
    paq, on the other hand, is very much a drop-in replacement. The only
    missing feature is specifying plugins by tags, but there was only one
    that used that.

--- roles/nvim/templates/lua/20_plugins.lua
+++ roles/nvim/templates/lua/20_plugins.lua
@@ -1,103 +1,98 @@
 -- vim: fdm=marker fmr=<<<,>>>
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+require "paq" {
+
+    -- paq can manage itself
+    'savq/paq-nvim',
 
     -- <<< General editing
     -- better status line
-    use 'nvim-lualine/lualine.nvim'
+    'nvim-lualine/lualine.nvim',
 
     -- better color scheme
-    use { "catppuccin/nvim", as = "catppuccin" }
+    { "catppuccin/nvim", as = "catppuccin" },
 
     -- Automatically surround by tags, brackets, ...
     --use 'tpope/vim-surround'
     -- Useful keybindings
-    use 'tpope/vim-unimpaired'
+    'tpope/vim-unimpaired',
 
     -- Auto-insert matching parentheses
-    use 'windwp/nvim-autopairs'
+    'windwp/nvim-autopairs',
 
     -- Access to file system - must-have on larger projects
-    use 'preservim/nerdtree'
+    'preservim/nerdtree',
 
     -- Comfortable code commenting
-    use 'preservim/nerdcommenter'
+    'preservim/nerdcommenter',
 
     -- Visual aid for buffer changes tree
-    use 'mbbill/undotree'
+    'mbbill/undotree',
 
     -- Sidebar with tags (variables and whatnot)
-    use 'preservim/tagbar'
+    'preservim/tagbar',
 
     -- remember last cursor position
-    use  'farmergreg/vim-lastplace'
+    'farmergreg/vim-lastplace',
 
     -- fuzzy-search for files, buffers, errors, ...
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
+    -- first one is a dependency
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
 
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 
     -- which-key - available completions for started command
-	use 'folke/which-key.nvim'
+    'folke/which-key.nvim',
 
     -- leap - quickly jump to any place displayed on a screen
-    use 'ggandor/leap.nvim'
+    'ggandor/leap.nvim',
     -- >>>
 
     -- <<< Programming
     -- easier access to terminals
-    use { "akinsho/toggleterm.nvim", tag = 'v2.*' }
+    "akinsho/toggleterm.nvim",
 
     -- Snippets
-    use 'dcampos/nvim-snippy'
-    use 'honza/vim-snippets'
+    'dcampos/nvim-snippy',
+    'honza/vim-snippets',
 
     -- LSP
     -- ready to use configs
-    use "williamboman/mason.nvim"
-    use "williamboman/mason-lspconfig.nvim"
-    use "neovim/nvim-lspconfig"
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
 
     -- Use lua scripts to provide LSP features
     -- contains wrappers for many command-line tools, like black
-    use 'nvimtools/none-ls.nvim'
+    'nvimtools/none-ls.nvim',
 
     -- nvim-cmp - expand LSP completions with data from other sources, like
     -- words in buffer, paths and snippets
     -- also, better keybinding for code completion in insert mode
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/nvim-cmp'
-    use 'dcampos/cmp-snippy'
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/nvim-cmp',
+    'dcampos/cmp-snippy',
 
     -- better function signature help
-    use 'ray-x/lsp_signature.nvim'
+    'ray-x/lsp_signature.nvim',
 
     -- treesitter understands your file while you edit it
     -- it's used for improved syntax highlighting, folding etc.
-    use {
+    {
         'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    }
+        build = function() require('nvim-treesitter.install').update({ with_sync = true }) end
+    },
 
     -- show diagnostics in window
-    use "folke/trouble.nvim"
+    "folke/trouble.nvim",
 
     -- Python PEP-8
-    use 'Vimjas/vim-python-pep8-indent'
+    'Vimjas/vim-python-pep8-indent',
 
     -- set commentstring for current part of file
-    use {
-        'JoosepAlviste/nvim-ts-context-commentstring',
-        requires = { {'nvim-treesitter/nvim-treesitter'} },
-    }
+    'JoosepAlviste/nvim-ts-context-commentstring',
     -- >>>
-
-end)
-
+}
